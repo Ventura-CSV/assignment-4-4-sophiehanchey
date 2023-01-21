166afb8fde7177b4cc229d965b283990eb1410da
Kyu Lee <kwooler@gmail.com>
2023-01-20 16:00:41 -0800

    A44

--- main.py
+++ main.py
@@ -2,7 +2,21 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    f
+    numbers = []
+    for i in range(5):
+        numbers.append(int(input('Enter a number: ')))
+        if i == 0 or minval > numbers[i]:
+            minval = numbers[i]
+        if i == 0 or maxval < numbers[i]:
+            maxval = numbers[i]
+
+    for i in range(5):
+        print(numbers[i], end=' ')
+    print()
+    print(f"The max value is {maxval} and min value is {minval}")
+
+    # print (max(numbers),  min(numbers))
+    # print (sorted(numbers))
 
     pass
 
--- main_test.py
+++ main_test.py
@@ -7,7 +7,7 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '15'
+    datastr = '10\n45\n50\n35\n25'
     sys.stdin = io.StringIO(datastr)
 
     main.main()
@@ -16,13 +16,21 @@ def test_main_1():
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    regex_string = r'[\w,\W]*1'
-    regex_string = r'[\w,\W]*1'
-    regex_string = r'[\w,\W]*1'
-    regex_string = r'[\w,\W]*1'
+    regex_string = r'[\w,\W]*10'
+    regex_string = r'[\w,\W]*45'
+    regex_string = r'[\w,\W]*50'
+    regex_string = r'[\w,\W]*35'
+    regex_string = r'[\w,\W]*25'
     regex_string += r'[\w,\W]*'
     print(regex_string)
     res = re.search(regex_string, lines[0])
+    assert res != None
+    print(res.group())
+    regex_string = r'[\w,\W]*50'
+    regex_string = r'[\w,\W]*10'
+    regex_string += r'[\w,\W]*'
+    print(regex_string)
+    res = re.search(regex_string, lines[1])
     assert res != None
     print(res.group())
 
@@ -30,7 +38,7 @@ def test_main_1():
 def test_main_2():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '7'
+    datastr = '-10\n33\n55\n20\n-5'
     sys.stdin = io.StringIO(datastr)
 
     main.main()
@@ -39,36 +47,20 @@ def test_main_2():
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    regex_string = r'[\w,\W]*1'
-    regex_string = r'[\w,\W]*1'
-    regex_string = r'[\w,\W]*1'
+    regex_string = r'[\w,\W]*-10'
+    regex_string += r'[\w,\W]*33'
+    regex_string += r'[\w,\W]*55'
+    regex_string += r'[\w,\W]*20'
+    regex_string += r'[\w,\W]*-5'
     regex_string += r'[\w,\W]*'
     print(regex_string)
     res = re.search(regex_string, lines[0])
     assert res != None
     print(res.group())
-
-
-def test_main_3():
-    captureOut = io.StringIO()
-    sys.stdout = captureOut
-    datastr = '16'
-    sys.stdin = io.StringIO(datastr)
-
-    main.main()
-    sys.stdout = sys.__stdout__
-    print('Captured ', captureOut.getvalue())
-    lines = captureOut.getvalue().split('\n')
-    print(lines)
-
-    regex_string = r'[\w,\W]*0'
-    regex_string = r'[\w,\W]*0'
-    regex_string = r'[\w,\W]*0'
-    regex_string = r'[\w,\W]*0'
-    regex_string = r'[\w,\W]*0'
-    regex_string = r'[\w,\W]*1'
+    regex_string = r'[\w,\W]*55'
+    regex_string += r'[\w,\W]*-10'
     regex_string += r'[\w,\W]*'
     print(regex_string)
-    res = re.search(regex_string, lines[0])
+    res = re.search(regex_string, lines[1])
     assert res != None
     print(res.group())
