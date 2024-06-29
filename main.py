29ffba6ac779c113c082558b76a59e2fba2fbb3a
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-28 22:44:57 -0700

    initial code, error in iteration

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 def main():
 
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    
+    # get the list
+    for i in range(5):
+        numbers.append(int(input('Please enter an integer: ')))
+        
+    # find min
+    for i in numbers:
+        for j in numbers:
+            if numbers[i] < numbers[j]:
+                minval = numbers[i]
+                print(minval)
+    
+    # find max          
+    for i in numbers:
+        for j in numbers:
+            if numbers[i] > numbers[j]:
+                maxval = numbers[i]
+                print(maxval)
+        
 
     print(*numbers)
-    print(maxval, minval)
+    # print(maxval, minval)
     ########################################
     # Do not delete the return statement
     ########################################
